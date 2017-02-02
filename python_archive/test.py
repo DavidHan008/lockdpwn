d20248dc894b9ab133dd906bb05f360939d987d7
edward <gyurse@gmail.com>
2017-02-02 17:50:49 +0900

    170202

--- python_archive/test.py
+++ python_archive/test.py
@@ -1,114 +1,137 @@
-#!/usr/bin/env pytohn
+#!/usr/bin/env python
 #-*- coding: utf-8 -*-
-'''
-	python ==> 연습용 코드 (계속 작성중...)
-               연립방정식, 역행렬을 푸는 프로그램
-'''
+
+import os
 import sys
-import subprocess
-from scipy.optimize  import fsolve
-from sympy  import Matrix, solve_linear_system
-from sympy import *
-from sympy.abc import x,y
-import math
+import sqlite3
+import _winreg
 
-def nmapHostFinder():
-	ret = subprocess.check_output("nmap -sP 192.168.24.0/24", shell=True)
+'''
+print "======== Chrome Histroy Viewer ============\n"
 
-	for line in ret:
-		if line == '\n':
-			continue
+os.system("taskkill /f /im chrome.exe")
 
+con = sqlite3.connect('C:\\Users\\vdl\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History')
+cursor = con.cursor()
+cursor.execute("SELECT * FROM keyword_search_terms ORDER BY url_id DESC")
 
-def solveODE(a, b):
+count = 0
 
-	return
+while count <= 5:
+    count += 1
+    data = cursor.fetchone()
+    covdata = "".join(str(data))
 
+    print "\n", covdata
 
-def solveSimultaneousEq(a,b,c,d,e,f):
-	system = Matrix(( (a,b,c) , (d,e,f) ))
-
-	print ""
-	print "--------------------------------"
-	print solve_linear_system(system, x, y)
-	print "--------------------------------"
-	print ""
+    if count == 15:
+        print ("\nComplete Histroy DB load - ",count ," Search keywords ")
 
 
-def getInverseMatrix(size,a,b,c,d,e=0,f=0,g=0,h=0,i=0):
-	size = int(size)
-	a = int(a)
-	b = int(b)
-	c = int(c)
-	d = int(d)
-	e = int(e)
-	f = int(f)
-	g = int(g)
-	h = int(h)
-	i = int(i)
+_reg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
+_key = _winreg.OpenKey(_reg, r"Software\\HNC\\Hwp\\9.0\\HwpFrame_KOR\\RecentFile", 0, _winreg.KEY_ALL_ACCESS)
 
-	if size == 2:
-		Z = Matrix(size, size, [a,b,c,d])
-		D = Z.det()
-		Zi = Z.inv()
-		DZi = simplify(D*Zi)
+try:
+    count = 0
+    while True:
+        name, value, type = _winreg.EnumValue(_key, count)
 
-	elif size == 3:
-		Z = Matrix(size, size, [a,b,c,d,e,f,g,h,i])
-		D = Z.det()
-		Zi = Z.inv()
-		DZi = simplify(D*Zi)
+        if isinstance(value, int):
+            print ""
+        else:
+            print name + " : " + value.decode('UTF-16').encode('cp949')
 
-	if DZi:
-		print ""
-		print "--------------------------------"
-		pprint(DZi)
-		print "--------------------------------"
-		print ""
+        count += 1
+
+except WindowsError:
+    pass
 
 
-def main():
-	while True:
-		print "1 : Simultaneous Equation"
-		print "2 : Inverse Matrix"
-		print "q to quit"
-		choose = raw_input("Choose Options: ")
+'''
 
-		if choose == '1':
-			print "solve Simultaneous Equation"
-			print "ax + by = c"
-			print "dx + ey = f"
-			ret = raw_input("type (a,b,c,d,e,f): ")
-			a,b,c,d,e,f = ret.split(',')
+def Word():
+    _reg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
+    _key = _winreg.OpenKey(_reg, r"Software\\Microsoft\\Office\\14.0\\Word\\File MRU", 0, _winreg.KEY_ALL_ACCESS)
 
-			solveSimultaneousEq(a,b,c,d,e,f)
-
-		elif choose == '2':
-		        print "solve Inverse Matrix"
-			print "[ a  b  c ]"
-			print "[ d  e  f ]"
-			print "[ g  h  i ]"
-			ret = raw_input("type (a,b,c,d,e,f,g,h,i): ")
-			size = raw_input("choose size: ")
-
-			if len(ret) >= 7 and len(ret) < 17:
-				a,b,c,d = ret.split(',')
-				getInverseMatrix(size,a,b,c,d)
-
-			elif len(ret) >= 17 and len(ret) < 50:
-				a,b,c,d,e,f,g,h,i = ret.split(',')
-				getInverseMatrix(size,a,b,c,d,e,f,g,h,i)
-			else:
-				print "[-] Not Valid"
-				print ""
+    try:
+        count = 0
+        while True:
+            name, value, type = _winreg.EnumValue(_key, count)
+            print  unicode(value,'euc-kr')
+            count += 1 
+    except  WindowsError:
+        pass
 
 
-				
-		elif choose == 'q' or choose == 'Q' or choose == 'exit' or choose == 'quit':
-			sys.exit(1)
+def Excel():
+    _reg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
+    _key = _winreg.OpenKey(_reg, r"Software\\Microsoft\\Office\\14.0\\Excel\\File MRU", 0, _winreg.KEY_ALL_ACCESS)
+
+    try:
+        count = 0
+        while True:
+            name, value, type = _winreg.EnumValue(_key, count)
+            print value
+            count += 1
+    except  WindowsError:
+        pass
+
+
+
+def PowerPoint( ):
+    _reg = _winreg.ConnectRegistry(None, _winreg.HKEY_CURRENT_USER)
+    _key = _winreg.OpenKey(_reg, r"Software\\Microsoft\\Office\\14.0\\PowerPoint\\File MRU", 0, _winreg.KEY_ALL_ACCESS)
+
+    try:
+        count = 0
+        while True:
+            name, value, type = _winreg.EnumValue(_key, count)
+            print  value
+            count += 1 
+    except  WindowsError:
+        pass
+ 
+
+
+Word()
+Excel()
+PowerPoint()
 
 
 
 
-if __name__ == '__main__':
-	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
