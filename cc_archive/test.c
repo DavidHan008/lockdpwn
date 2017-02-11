7d75f89d098d856e06653f49bb61af8e5e994d64
edward <gyurse@gmail.com>
2017-02-12 02:57:43 +0900

    170212

--- cc_archive/test.c
+++ cc_archive/test.c
@@ -1,8 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-4203f2c0 : system()
-42029bb0 : exit()
-420d7920 : setreuid()
+const int kCmdLen = 400;
 
-42127ea4 : /bin/sh
+void print_dir_with_system(char *path)
+{
+	char cmd[kCmdLen];
+	const char* cmd_prefix = "dir";
+	if (strlen(cmd_prefix) + strlen(path) +1 < kCmdLen)
+	{
+		 _snprintf(cmd, kCmdLen, "%s %s", cmd_prefix, path);
+	}
+	system(cmd);
+}
 
-0804849d : poppopret
+int main(int argc, const char *argv[])
+{
+	char *exec_file = argv[0];
+	char *path = argv[1];
+	
+	if (argc == 2) 
+	{
+		print_dir_with_system(path);
+	}
+	else 
+	{
+		printf("Usage > %s [filename]\n", exec_file);
+	}
+
+	return 0;
+}
