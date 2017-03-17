2f18a269e0e0b93eb25912c35621308258412f21
edward <gyurse@gmail.com>
2017-03-17 17:14:44 +0900

    from windows

--- cpp_archive/only_for_windows/StickyNotes/ProcessManagement.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-
-extern int GetTimeStr (char timeStr[]);
-extern bool FindProcess(CString processName);
-extern bool KillProcess(CString processName);
-extern bool StartProcess(CString m_Process);
-
