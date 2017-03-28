dc54f3f191d4513f300a9c161079dc6acf09a971
lockdpwn <gyurse@gmail.com>
2017-03-28 14:35:09 +0900

    from windows

--- cpp_archive/RecursiveFactorial.cpp
+++ cpp_archive/RecursiveFactorial.cpp
@@ -1,24 +1,36 @@
-#include "stdafx.h"
-
+#include <stdio.h>
 
 int factorial(int n)
 {
-	if (n == 0)
+	if ( n == 0 )
 	{
 		return 1;
 	}
-	else
-		return n * factorial(n - 1);
+	else 
+	{
+		return n * factorial(n-1);
+	}
 }
 
-
-void main()
+int main(int argc, const char *argv[])
 {
-	printf("1! = %d\n", factorial(1));
-	printf("2! = %d\n", factorial(2));
 	printf("3! = %d\n", factorial(3));
 	printf("4! = %d\n", factorial(4));
-	printf("5! = %d\n", factorial(5));
-	printf("6! = %d\n", factorial(6));
+	printf("9! = %d\n", factorial(9));
 
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- cpp_archive/recursiveBinarySearch.cpp
+++ cpp_archive/recursiveBinarySearch.cpp
@@ -1,59 +1,56 @@
-#include "stdafx.h"
-
+#include <stdio.h>
 
 int BsearchRecur(int ar[], int first, int last, int target)
 {
 	int mid;
-
-	// printf("[%d]<%d>", first,last);
-
-	if (first > last)
+	if (first > last) 
 	{
-
 		return -1;
 	}
+	mid = (first + last) /2;
 
-	mid = (first + last) / 2;
-
-	if (ar[mid] == target)
+	if (ar[mid] == target) 
 	{
 		return mid;
 	}
-
-	else if (target < ar[mid])
-		return BsearchRecur(ar, first, mid - 1, target);
-	else
-		return BsearchRecur(ar, mid + 1, last, target);
-
+	else if (ar[mid] > target)
+	{
+		 return BsearchRecur(ar, first, mid - 1, target);
+	}
+	else if (ar[mid] < target)
+	{
+		 return BsearchRecur(ar, mid + 1, last, target);
+	}
 }
 
-void main()
+int main(int argc, const char *argv[])
 {
-	int arr[] = { 1, 3, 5, 7, 9 };
+	int arr[] = {1,3,5,7,9};
 	int idx;
 
-
-	idx = BsearchRecur(arr, 0, sizeof(arr) / sizeof(int)-1, 7);
-
-	if (idx == -1)
+	idx = BsearchRecur(arr, 0, sizeof(arr)/sizeof(int)-1, 7);
+	if (idx == -1) 
 	{
-		printf("Å½»ö ½ÇÆĞ\n");
+		printf("fail\n");
 	}
-	else
+	else 
 	{
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d \n", idx);
+		printf("target index : %d\n", idx);
 	}
 
-
-	idx = BsearchRecur(arr, 0, sizeof(arr) / sizeof(int)-1, 4);
-
-	if (idx == -1)
+	idx = BsearchRecur(arr, 0, sizeof(arr)/sizeof(int)-1, 4);
+	if (idx == -1) 
 	{
-		printf("Å½»ö ½ÇÆĞ\n");
+		printf("fail\n");
 	}
-	else
+	else 
 	{
-		printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d \n", idx);
+		printf("target index : %d\n", idx);
 	}
 
+
+	return 0;
 }
+
+
+
