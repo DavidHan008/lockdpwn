aa6ae502f1a519b5c229b8789fe96ce58c98019a
edward <gyurse@gmail.com>
2017-04-14 00:46:43 +0900

    from windows

--- cpp_archive/test.cpp
+++ cpp_archive/test.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 #include <string>
-
-#define PI 3.14159
 using namespace std;
 
-class Circle {
-	int radius;
+class Person{
+  string name;
+  string tel;
 
-public:
-	void setRadius(int radius);
-	double getArea();
+ public:
+  Person() {}
+  string getName() { return name; }
+  string getTel() { return tel; }
+  void set(string name, string tel);
 };
 
-void Circle::setRadius(int radius) {
-	this->radius = radius;
+void Person::set(string name, string tel){
+  this->name = name;
+  this->tel = tel;
 }
 
-double Circle::getArea() {
-	return this->radius * this->radius * PI;
+int main(int argc, const char *argv[]){
+  int i = 0;
+  Person *p = new Person[3];
+  string str[3];
+  string name;
+
+  cout << "이름과 전화번호를 입력해 주세요" << endl;
+  cout << "사람 1>> ";
+  getline(cin, str[0], '\n');
+  cout << "사람 2>> ";
+  getline(cin, str[1], '\n');
+  cout << "사람 3>> ";
+  getline(cin, str[2], '\n');
+
+  p[0].set(str[0].substr(0,9), str[0].substr(10,13));
+  p[1].set(str[1].substr(0,9), str[1].substr(10,13));
+  p[2].set(str[2].substr(0,9), str[2].substr(10,13));
+
+  cout << "모든 사람의 이름은 " << p[0].getName() << ", " << p[1].getName() << ", " << p[2].getName() << endl;
+  cout << "전화번호를 검색합니다. 이름을 입력하세요 >> ";
+  cin >> name;
+
+  for (i = 0; i < 3; i++)
+    if (name == p[i].getName())
+      break;
+
+  cout << "전화번호는 " << p[i].getTel();
+  return 0;
 }
-
-int main(int argc, const char *argv[]) {
-	int numCircle;
-	int rad;
-	int count = 0;
-	cout << "원의 개수 >> ";
-	cin >> numCircle;
-
-	Circle *c = new Circle[numCircle];
-
-	for (int i = 1; i <= numCircle; i++){
-		cout << "원" << i << "의 반지름 >> ";
-		cin >> rad;
-		c[i - 1].setRadius(rad);
-	}
-
-	for (int j = 0; j < numCircle; j++){
-		if (c[j].getArea() >= 100)
-			count++;
-	}
-
-	cout << "면적이 100보다 큰 원은 " << count << "개 입니다" << endl;
-	return 0;
-}
-
--- zet_cetera/tmux_setting/.bashrc
+++ zet_cetera/tmux_setting/.bashrc
@@ -175,5 +175,3 @@ tmux attach-session -d'
 if [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh ]; then
     source /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
 fi
-
-
