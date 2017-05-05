5e4aa2a1f730725e1c95e5e9c2dfdde406e424fb
lockdpwn <gyurse@gmail.com>
2017-05-05 22:29:15 +0900

    from windows

--- cpp_archive/ros/catkin_ws/build/my_first_ros_pkg/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/edward/ros/catkin_ws/src/my_first_ros_pkg/include".split(';') if "/home/edward/ros/catkin_ws/src/my_first_ros_pkg/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "my_first_ros_pkg"
-PROJECT_SPACE_DIR = "/home/edward/ros/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- embedded_archive (Arduino,RasPi,Freescale..)/ROS_arduino/arduinoROSTest.ino
+++ embedded_archive (Arduino,RasPi,Freescale..)/ROS_arduino/arduinoROSTest.ino
@@ -1,24 +1,36 @@
 /*
- *  arduino ==> ROS
+ *  arduino ==> ROS, 초음파센서의 값을 오드로이드로 퍼블리시하고 
+                     서보모터값을 오드로이드로부터 서브스크라이브 하는 코드
+					 
+					 arduino_test 패키지와 같이 실행된다
+					 
+					 rosrun rosserial_python serial_node
+					 rosrun arduino_test arduino_test_subscriber
  */
 #include <ros.h>
 #include <std_msgs/Int32.h>  // 거리값을 전송하기 위한 헤더 선언
 #include "Servo.h"
 
+// 서보모터와 핀 값들을 선언한다
 Servo myservo;
-
 const int trigPin = 7;
 const int echoPin = 6;
 long duration = 0;
 int distance = 0;
 
+// ROS 관련 변수들
 ros::NodeHandle  nh;
 std_msgs::Int32 int_msg;
+
+// 초음파센서 값을 오드로이드로 전달하는 퍼블리셔 선언
 ros::Publisher dist("getUltrasonicData", &int_msg);
 
+// 서보모터값을 입력받는 서브스크라이버 콜백함수 선언
 void messageCb(const std_msgs::Int32& angle_msg){
 	myservo.write(angle_msg.data);
 }
+
+// 서보모터 값을 오드로이드로부터 전달받는 서브스크라이버 선언
 ros::Subscriber<std_msgs::Int32> sub("setServoData", messageCb );
 
 // 초음파센서의 거리를 계산해서 반환하는 함수
