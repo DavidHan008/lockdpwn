ea817d458492030a569c3544caa2f168d0fbbc6e
lockdpwn <gyurse@gmail.com>
2017-05-14 03:11:05 +0930

    from Odroid

--- ROS/catkin_ws/src/imu_test/src/imu_test_node.cpp
+++ ROS/catkin_ws/src/imu_test/src/imu_test_node.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 using namespace ros;
 
-<<<<<<< HEAD
 const double alpha = 0.5;
 double fXg_old =0;
 double Xg, Yg, Zg;
@@ -26,27 +25,13 @@ void getImuData(const sensor_msgs::Imu& imu){
 
 	//ROS_INFO("orientation_cov : [%lf, %lf, %lf]",imu.orientation_covariance[0], imu.orientation_covariance[1], imu.orientation_covariance[2]);
 
-	//ROS_INFO("angular vel x : [%lf]", imu.angular_velocity.x);
-	//ROS_INFO("angular vel y : [%lf]", imu.angular_velocity.y);
-	//ROS_INFO("angular vel z : [%lf]", imu.angular_velocity.z);
-=======
-// IMU 센서에서 데이터를 서브스크리브 하는 함수
-void getImuData(const sensor_msgs::Imu& imu){
-	// ROS_INFO("x : [%lf]", imu.orientation.x);
-	// ROS_INFO("y : [%lf]", imu.orientation.y);
-	// ROS_INFO("z : [%lf]", imu.orientation.z);
-
-	//ROS_INFO("orientation_cov : [%lf, %lf, %lf]",imu.orientation_covariance[0], imu.orientation_covariance[1], imu.orientation_covariance[2]);
-
-	ROS_INFO("angular vel x : [%lf]", imu.angular_velocity.x);
-	ROS_INFO("angular vel y : [%lf]", imu.angular_velocity.y);
-	ROS_INFO("angular vel z : [%lf]", imu.angular_velocity.z);
->>>>>>> 66322ec3bea54a0e38d73c43d964b026548dbb59
+//	ROS_INFO("angular vel x : [%lf]", imu.angular_velocity.x);
+//	ROS_INFO("angular vel y : [%lf]", imu.angular_velocity.y);
+//	ROS_INFO("angular vel z : [%lf]", imu.angular_velocity.z);
 
 	//ROS_INFO("linear acc x : [%lf]", imu.linear_acceleration.x);
 	//ROS_INFO("linear acc y : [%lf]", imu.linear_acceleration.y);
 	//ROS_INFO("linear acc z : [%lf]", imu.linear_acceleration.z);
-<<<<<<< HEAD
 
 	// 현재 시간을 출력하는 코드 
 	//Time time = Time::now();
@@ -59,15 +44,13 @@ void getImuData(const sensor_msgs::Imu& imu){
 	fXg = Xg * alpha + (fXg * (1.0 - alpha));
 	fYg = Yg * alpha + (fYg * (1.0 - alpha));
 	fZg = Zg * alpha + (fZg * (1.0 - alpha));
-	
+
 	accSumVector = sqrt(pow(fXg,2) + pow(fYg,2) + pow(fZg,2));
 
 	// 소수점 자리를 2자리수로 고정시킨다
 	cout << fixed;
 	cout.precision(2);
 	cout << "linear acc : [" << fXg << " " << fYg << " " << fZg << "], accSumVector : [ "  << accSumVector << " ]" <<endl;
-=======
->>>>>>> 66322ec3bea54a0e38d73c43d964b026548dbb59
 }
 
 int main(int argc, char **argv){
