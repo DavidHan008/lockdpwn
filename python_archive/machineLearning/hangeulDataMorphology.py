82dc28bf5660b99c580cbcfb099ed74cd1a74a11
lockdpwn <gyurse@gmail.com>
2017-05-16 23:08:33 +0900

    from windows

--- python_archive/machineLearning/hangeulDataMorphology.py
+++ python_archive/machineLearning/hangeulDataMorphology.py
@@ -9,88 +9,93 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 한글 데이터를 불러온다
-with h5py.File('D:\\googleDrive\\private2\\dataset_ML\\visualComputing_hangeul\\kalph_train.hf', 'r') as hf:
-    images = np.array(hf['images'])
-    labels = np.array(hf['labels'])
+def getDataFromFile():
+    # 한글 데이터를 불러온다
+    with h5py.File('D:\\googleDrive\\private2\\dataset_ML\\visualComputing_hangeul\\kalph_train.hf', 'r') as hf:
+        images = np.array(hf['images'])
+        labels = np.array(hf['labels'])
+    
+def getLenArray(img):
+    # 데이터의 길이를 변수로 받는다
+    len_image = len(img)
 
-# 필터 kernel을 설정한다
-kernel = np.ones((2,2), np.uint8)
 
-len_image = len(images)
 
+def applyRandomMorphologyEffect():
+    # images 이미지들을 랜덤으로 모폴로지 연산을 수행한다
+    for i in range(0, len_image):
+        rNum = random.randrange(1,8)
+
+        if(rNum == 1):
+            modified_images[i] = cv2.erode(images[i],kernel,iterations = 1)
+        elif(rNum == 2):
+            modified_images[i] = cv2.dilate(images[i],kernel,iterations = 1)
+        elif(rNum == 3):
+            modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_OPEN, kernel)
+        elif(rNum == 4):
+            modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_GRADIENT, kernel)
+        elif(rNum == 5):
+            modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_CLOSE, kernel)
+        elif(rNum == 6):
+            modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_TOPHAT, kernel)
+        elif(rNum == 7):
+            modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_BLACKHAT, kernel)
+
+def showImage():
+    # 모폴로지 연산 후와 전의 이미지를 비교해본다 
+    for i in range(0,10):
+        fig = plt.figure()
+        fig.add_subplot(1,2,1)
+        plt.imshow(images[i], cmap='gray_r') ; plt.axis('off')
+        fig.add_subplot(1,2,2)
+        plt.imshow(modified_images[i], cmap='gray_r') ; plt.axis('off')
+        plt.show()
+
+
+def shuffleData(modified_images, modified_labels):
+    # 이미지들을 랜덤순서로 섞는다
+    shuf = np.arange(len_image)
+    np.random.shuffle(shuf)
+    modified_images = modified_images[shuf]
+    modified_labels = modified_labels[shuf]
+
+
+def initProcess():
+    # 필터 kernel을 설정한다
+    kernel = np.ones((2,2), np.uint8)
+
+    # 데이터를 받기 위해 초기설정을 한다
+    modified_images = np.array(np.zeros(52998400).reshape(19600,52,52))
+    modified_labels = labels
 
 # 데이터를 19600x52x52 처럼 3차원 배열데이터로 만들어주는 코드들(사용안함)
 # aa = np.dstack((images[0], images[1])).swapaxes(0,2)
 # bb = np.array([images[0],images[1]])
 
-modified_images = np.array(np.zeros(52998400).reshape(19600,52,52))
-modified_labels = labels
 
-# images 이미지들을 랜덤으로 모폴로지 연산을 수행한다
-for i in range(0, len_image):
-    rNum = random.randrange(1,8)
-
-    if(rNum == 1):
-        modified_images[i] = cv2.erode(images[i],kernel,iterations = 1)
-    elif(rNum == 2):
-        modified_images[i] = cv2.dilate(images[i],kernel,iterations = 1)
-    elif(rNum == 3):
-        modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_OPEN, kernel)
-    elif(rNum == 4):
-        modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_GRADIENT, kernel)
-    elif(rNum == 5):
-        modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_CLOSE, kernel)
-    elif(rNum == 6):
-        modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_TOPHAT, kernel)
-    elif(rNum == 7):
-        modified_images[i] = cv2.morphologyEx(images[i], cv2.MORPH_BLACKHAT, kernel)
-    else:
-        continue
-
-
-# 모폴로지 연산 후와 전의 이미지를 비교해본다 
-for i in range(0,10):
-    fig = plt.figure()
-    fig.add_subplot(1,2,1)
-    plt.imshow(images[i], cmap='gray_r') ; plt.axis('off')
-    fig.add_subplot(1,2,2)
-    plt.imshow(modified_images[i], cmap='gray_r') ; plt.axis('off')
-    plt.show()
+def saveh5pyData(path, modified_images, modified_labels):
+    # hdf5 파일 포맷으로 위의 데이터를 저장한다 :-)
+    f = h5py.File('D:\\edward\\modified_file.hf','w')
+    f.create_dataset('modified_images',data=modified_images, compression_opts=9, compression='gzip')
+    f.create_dataset('modified_labels',data=modified_labels, compression_opts=9, compression='gzip')
+    f.close()
 
 
 
-# 이미지들을 랜덤순서로 섞는다
-shuf = np.arange(len_image)
-np.random.shuffle(shuf)
-modified_images = modified_images[shuf]
-modified_labels = modified_labels[shuf]
+def loadData():
+    # 저장된 데이터를 불러온다
+    g = h5py.File('D:\\edward\\modified_file.hf','r')
 
+    # 키를 확인한다
+    for keys in g:
+        print(keys, "---------->" , g[keys])
 
-# hdf5 파일 포맷으로 위의 데이터를 저장한다 :-)
-f = h5py.File('D:\\edward\\modified_file.hf','w')
+    mod_images = np.array(g['modified_images'])
+    mlabels = np.array(g['modified_labels'])
 
-f.create_dataset('modified_images',data=modified_images, compression_opts=9, compression='gzip')
-f.create_dataset('modified_labels',data=modified_labels, compression_opts=9, compression='gzip')
-
-f.close()
-
-
-
-# 저장된 데이터를 불러온다
-g = h5py.File('D:\\edward\\modified_file.hf','r')
-
-# 키를 확인한다
-for keys in g:
-    print(keys, "---------->" , g[keys])
-
-mod_images = np.array(g['modified_images'])
-mlabels = np.array(g['modified_labels'])
-
-
-# Image 데이터와 Label 데이터를 numpy 데이터로 수정한다
-mod_images = mod_images.reshape(19600, 2704, )
-mod_images = mod_images / 255.
+    # Image 데이터와 Label 데이터를 numpy 데이터로 수정한다
+    mod_images = mod_images.reshape(19600, 2704, )
+    mod_images = mod_images / 255.
 
 
 # train Label 데이터를 [1 x 100] 의 행렬로 표현한다
