e5d41e35a181746716c8dc4346c62dcdc11c2925
lockdpwn <gyurse@gmail.com>
2017-06-02 02:39:45 +0930

    from Odroid

--- ROS/catkin_ws/src/odroid_test/src/odroid_test_node.py
+++ ROS/catkin_ws/src/odroid_test/src/odroid_test_node.py
@@ -9,15 +9,14 @@ def getUltra(data):
 	ult = data.data
 
 
-def rosTest():
-	rospy.init_node('odroid_py_test_node', anonymous=True)
-	rate = rospy.Rate(10)
-	rospy.Subscriber('getUltraData', Float32, getUltra)
-	pub = rospy.Publisher('setServoAngle', Int32, queue_size=10)
+rospy.init_node('odroid_py_test_node', anonymous=True)
+rate = rospy.Rate(10)
+rospy.Subscriber('getUltraData', Int32, getUltra)
+pub = rospy.Publisher('setServoAngle', Int32, queue_size=10)
 
-rosTest()
 while not rospy.is_shutdown():
-	if ult > 2.5 and ult < 12.5:
 		pub.publish(ult)
 		rate.sleep()
+		print "ult : " , ult
+
 
