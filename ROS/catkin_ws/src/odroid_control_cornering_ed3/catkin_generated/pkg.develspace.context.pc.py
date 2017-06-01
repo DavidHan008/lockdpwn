10fc2bf5a41e87b95d7b0aca1462235a6d2871e7
lockdpwn <gyurse@gmail.com>
2017-06-02 00:03:47 +0930

    from Odroid

---
+++ ROS/catkin_ws/src/odroid_control_cornering_ed3/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/odroid/gitrepo/lockdpwn/ROS/catkin_ws/src/odroid_control_cornering_ed3/include".split(';') if "/home/odroid/gitrepo/lockdpwn/ROS/catkin_ws/src/odroid_control_cornering_ed3/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "odroid_control_cornering_ed3"
+PROJECT_SPACE_DIR = "/home/odroid/gitrepo/lockdpwn/ROS/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
--- ROS/catkin_ws/src/odroid_control_cornering_speed_ed/src/odroid_control_cornering_ed_node.cpp
+++ ROS/catkin_ws/src/odroid_control_cornering_speed_ed/src/odroid_control_cornering_ed_node.cpp
@@ -11,29 +11,32 @@
 #include <std_msgs/Float64.h>
 #include "pid.h"
 
-#define TARGET_VAL 1535
-#define ARRAYNUM 20
+#define TARGET_VAL 1520
+#define ARRAYNUM 15
+#define ARRAYNUM2 15
 
 using namespace std;
 using namespace ros;
 
 int cnt = 0;
+int cnt2 = 0;
 
 double ult = 0;
 
-double P = 25;
-double I = 1;
-double D = 15;
+double P = 0;
+double I = 0;
+double D = 0;
 
-double P2 = 0.6;
-double I2 = 0;
-double D2 = 0;
+double P2 = 5;
+double I2 = .5;
+double D2 = .5;
 
 double v_x = 0;
 double Ww_Reff = 0;
 double slip_ratio = 0;
 double err = 0;
 double u = 0;
+double uu[ARRAYNUM] = {0};
 double dt = 100;
 double threshold = 0;
 double desired_slip_ratio = 0.0;
@@ -43,16 +46,19 @@ double v = 0;
 double vfinal = 0;
 double vv[ARRAYNUM] = {0};
 
+double ww[ARRAYNUM2] = {0};
+double wfinal = 0;
+
 double aZ;
 double faZ;
 double alpha = 0.5;
 
 double Yaw;
 double fYaw;
-double eYaw;
 
-double firstValue;
+double firstValue = 0;
 int counter = 0;
+int target_val_final = TARGET_VAL;
 
 // servo & dc 모터 퍼블리셔에 전달할 변수 선언
 std_msgs::Int32 servo;
@@ -81,6 +87,14 @@ double getMean(double dist[]) {
   return sum / ARRAYNUM;
 }
 
+// 아두이노로부터 초음파센서 값을 Subscribe하는 함수
+void ultra_dist(const std_msgs::Float32& ultra){
+	ult = ultra.data;
+
+	if(ult > 100) ult = 100;
+
+}
+
 // FR 홀센서 값을 받아 처리하는 콜백함수
 void hall_front(const std_msgs::Float32& h_front){
 	//cout << "                hall_front is : " << h_front.data << endl;
@@ -98,21 +112,19 @@ void hall_rear(const std_msgs::Float32& h_rear){
 void getAngleVel_z(const sensor_msgs::Imu& imu){
 	aZ = imu.angular_velocity.z;
 	faZ = aZ * alpha + (faZ * (1.0 - alpha));
-	
+
+	counter++;
+	if(counter == 30) firstValue = faZ;
 }
 
 void getYaw(const std_msgs::Float64 yaw){
-	fYaw = yaw.data;
+	Yaw = yaw.data;
 
-	//fYaw = Yaw * alpha + (fYaw * (1.0 - alpha));
-
-	counter++;
-	if(counter == 15) firstValue = fYaw;
-
+	fYaw = Yaw * alpha + (fYaw * (1.0 - alpha));
 }
 
 int main(int argc, char **argv){
-	init(argc, argv, "odroid_control_ed_node");
+	init(argc, argv, "odroid_control_cornering_ed_node");
 	NodeHandle nh;
 
 	// servo & motor 값을 처리하는 퍼블리셔 선언
@@ -121,11 +133,12 @@ int main(int argc, char **argv){
 	Rate loop_rate(10);  // 10 Hz
 
 	// 초음파 & 홀센서 2개 값을 처리하는 섭스크라이버 선언
+	Subscriber ultra_sub = nh.subscribe("dist", 1, ultra_dist);
 	Subscriber hall_front_sub = nh.subscribe("Rps_front", 1, hall_front);
 	Subscriber hall_rear_sub = nh.subscribe("Rps_rear", 1, hall_rear);
 
-//	Subscriber imudata = nh.subscribe("imu/data", 1, getAngleVel_z);
-	Subscriber imudata = nh.subscribe("imu/yaw_ed", 1, getYaw);
+	Subscriber imudata = nh.subscribe("imu/data", 1, getAngleVel_z);
+	//Subscriber imudata = nh.subscribe("imu/yaw_ed", 1, getYaw);
 
 	PID pid_dcmotor(P , I , D);
 	PID pid_yawrate(P2, I2, D2);
@@ -139,31 +152,24 @@ int main(int argc, char **argv){
 
 		err = slip_ratio - desired_slip_ratio;
 		u = pid_dcmotor.update(err, dt);
+	
+		faZ -= firstValue / 2;
 
-		//err_angvel_z = -faZ;
-		if(fYaw * firstValue > 0){
-			eYaw = fYaw - firstValue;
-		}
-		else{	
-			if(firstValue < 0)
-				eYaw = -360 - firstValue + fYaw;
-			else
-				eYaw =  +firstValue - fYaw;
-		}
-		
-		err_angvel_z = -eYaw;
+		if(firstValue != 0)
+			err_angvel_z = faZ;
+		else
+			err_angvel_z = 0;
+
+		//fYaw -= firstValue / 2;
+		//err_angvel_z = -fYaw;
 
 		v =  pid_yawrate.update(err_angvel_z, dt);
 		
-		if(v > 25) v = 25;
-		else if(v < -25) v = -25;
+		if(v > 20) v = 20;
+		else if(v < -20) v = -20;
 
-		vv[cnt++] = v;
+		vv[cnt++] = -v;
 		vfinal = getMean(vv);
-
-//		if(eYaw < 10 && eYaw > -10)
-//			vfinal = 0;
-
 
 		if(cnt > ARRAYNUM) cnt = 0;
 
@@ -172,23 +178,32 @@ int main(int argc, char **argv){
 
 		if(u > 70) u = 70;
 
-		dc_motor.data = TARGET_VAL + u;
+		dc_motor.data = target_val_final + u;
 
-		if(vfinal > 10) vfinal = 10;
-		if(vfinal < -10) vfinal = -10;
+		if(counter > 40) 
+			if(target_val_final > 1506)
+					target_val_final -= 2;
 
-		servo.data = 65 + vfinal;
-		//servo.data = 80;
+		if(vfinal > 20) vfinal = 20;
+		if(vfinal < -20) vfinal = -20;
+
+		servo.data = 76 + vfinal;
+		//servo.data = 75;
 		
-		cout << servo << ",  " << dc_motor << endl;
-		cout << "fYaw is:  " << fYaw <<", eYaw is : "<< eYaw  << ", and firstValue is : " << firstValue << endl;
 
-		cout <<"vfinal is ; " << vfinal << endl;
+		ww[cnt2++] = ult;
+		wfinal = getMean(ww);
 
+		if(wfinal > 5 && wfinal < 90) dc_motor.data -= 2;
 
-		cout << endl << endl;
+		if(cnt2 > ARRAYNUM2) cnt2 = 0;
+
 		servo_pub.publish(servo);		
 		motor_pub.publish(dc_motor);		
+
+		cout << servo << ",  " << dc_motor << endl;
+		cout << "faZ is : " << faZ <<", fYaw is : "<< fYaw  << ", and firstValue is : " << firstValue << endl;
+		cout << wfinal << endl;
 
 		loop_rate.sleep();
 		spinOnce();
