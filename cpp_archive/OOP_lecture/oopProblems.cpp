4cebcaad1f2717c849f3b3849bd67fda3e6a2726
lockdpwn <gyurse@gmail.com>
2017-06-05 09:41:53 +0900

    from windows

--- cpp_archive/OOP_lecture/oopProblems.cpp
+++ cpp_archive/OOP_lecture/oopProblems.cpp
@@ -12,14 +12,166 @@
 
 
 ------------------------------------------------------
+/*
+ * c++ ==> 객체지향 예제 12-3, get()을 이용해 텍스트 파일 읽기
+ */
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+	char* file = "c:\\windows\\system.ini";
+
+	ifstream fin(file);
+	if(!fin){
+		cout << file << " 열기 오류 " << endl;
+		return 0;
+	}
+
+	int count = 0;
+	int c;
+	while((c=fin.get()) != EOF){
+		cout << (char)c;
+		count++;
+	}
+
+	cout << "읽은 바이트 수는 " << count << endl;
+	fin.close();
+
+	return 0;
+}
 
 
 
 ------------------------------------------------------
+/*
+ * c++ ==> 객체지향 예제 12-1, 키보드로 입력받아 텍스트 파일 저장하기
+ */
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]){
+	
+	char name[10], dept[20];
+	int sid;
+
+	cout << "이름>> ";
+	cin >> name;
+	cout << "학번>> ";
+	cin >> sid;
+	cout << "학과>> ";
+	cin >> dept;
+
+	ofstream fout("c:\\student.txt");
+	if(!fout){
+		cout << "student.txt 파일을 열 수 없다";
+	}
+
+	fout << name << endl;
+	fout << sid << endl;
+	fout << dept << endl;
+
+	fout.close();
+
+	return 0;
+}
+
+
 
 
 
 ------------------------------------------------------
+/*
+ * c++ ==> 객체지향 p410 5,6 , BaseArray 클래스를 상속받는 MyQueue 클래스와 MyStack 클래스를 작성하고 사용해 본 코드
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BaseArray {
+private:
+	int capacity;
+	int *mem;
+
+protected:
+	BaseArray(int capacity=100){
+		this->capacity = capacity; 
+		mem = new int[capacity];
+	}
+	~BaseArray() { delete[] mem; }
+
+	void put(int index, int val) { mem[index] = val; }
+	int get(int index) { return mem[index]; }
+	int getCapacity() { return capacity; }
+};
+
+class MyQueue : public BaseArray {
+	int count;
+public:
+	MyQueue(int cap) : BaseArray(cap) {
+		count = 0;
+	}
+
+	int capacity();
+	void enqueue(int val);
+	int dequeue();
+	int length();
+};
+
+int MyQueue::capacity(){
+	return getCapacity();
+}
+
+void MyQueue::enqueue(int val){
+	put(count++, val);
+}
+
+int MyQueue::dequeue(){
+	return get(--count);
+}
+
+int MyQueue::length(){
+	return count;
+}
+
+class MyStack : public BaseArray {
+
+public:
+		
+};
+
+int main(int argc, const char *argv[]){
+	MyQueue mQ(100);
+
+	int n;
+	cout << "큐에 삽입할 5개의 정수를 입력하라>> ";
+
+	for(int i =0 ; i< 5; i++){
+		cin >> n;
+		mQ.enqueue(n);
+	}
+
+	cout << "큐의 용량: " << mQ.capacity() << ", 큐의 크기: " << mQ.length() << endl;
+
+	cout << "큐의 원소를 순서대로 제거하여 출력한다>> ";
+
+	while(mQ.length() != 0){
+		cout << mQ.dequeue() << ' ';
+	}
+
+	cout << endl << "큐의 현재 크기 : " << mQ.length() << endl;
+
+	return 0;
+}
+
+
+
 
 
 
--- cpp_archive/test.cpp
+++ cpp_archive/test.cpp
@@ -1,87 +1,36 @@
 /*
- * c++ ==> 객체지향 p410 5,6 , BaseArray 클래스를 상속받는 MyQueue 클래스와 MyStack 클래스를 작성하고 사용해 본 코드
+ * c++ ==> 객체지향 예제 
  */
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-class BaseArray {
-private:
-	int capacity;
-	int *mem;
-
-protected:
-	BaseArray(int capacity=100){
-		this->capacity = capacity; 
-		mem = new int[capacity];
-	}
-	~BaseArray() { delete[] mem; }
-
-	void put(int index, int val) { mem[index] = val; }
-	int get(int index) { return mem[index]; }
-	int getCapacity() { return capacity; }
-};
-
-class MyQueue : public BaseArray {
-	int count;
-public:
-	MyQueue(int cap) : BaseArray(cap) {
-		count = 0;
-	}
-
-	int capacity();
-	void enqueue(int val);
-	int dequeue();
-	int length();
-};
-
-int MyQueue::capacity(){
-	return getCapacity();
-}
-
-void MyQueue::enqueue(int val){
-	put(count++, val);
-}
-
-int MyQueue::dequeue(){
-	return get(--count);
-}
-
-int MyQueue::length(){
-	return count;
-}
-
-class MyStack : public BaseArray {
-
-public:
-		
-};
-
 int main(int argc, const char *argv[]){
-	MyQueue mQ(100);
+	char* file = "c:\\test.txt";
 
-	int n;
-	cout << "큐에 삽입할 5개의 정수를 입력하라>> ";
-
-	for(int i =0 ; i< 5; i++){
-		cin >> n;
-		mQ.enqueue(n);
+	ifstream fin(file);
+	if(!fin){
+		cout << file << " 열기 오류 " << endl;
+		return 0;
 	}
 
-	cout << "큐의 용량: " << mQ.capacity() << ", 큐의 크기: " << mQ.length() << endl;
+	int count = 0;
+	int c;
+	int lineNum = 1;
 
-	cout << "큐의 원소를 순서대로 제거하여 출력한다>> ";
+	while((c=fin.get()) != EOF){
+		cout << lineNum++ << " : " << (char)c;
+		if(c == '\n') 
+			cout << lineNum++ << " : " << (char)c;
 
-	while(mQ.length() != 0){
-		cout << mQ.dequeue() << ' ';
+		cout << (char)c;
+		count++;
 	}
 
-	cout << endl << "큐의 현재 크기 : " << mQ.length() << endl;
+	cout << "읽은 바이트 수는 " << count << endl;
+	fin.close();
 
 	return 0;
 }
-
-
-
-
