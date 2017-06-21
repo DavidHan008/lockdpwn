ea33faed08d3f45a7075d8ff1d6a33058076cf9b
lockdpwn <gyurse@gmail.com>
2017-06-22 01:50:16 +0900

    from windows

--- python_archive/vpython_Test14.py
+++ python_archive/vpython_Test14.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 '''
-    python ==> vpython
+    python ==> vpython을 사용해 2개의 진자의 운동을 애니메이션 해 본 코드 
+ 
+             code from https://youtu.be/oMBohUQz3NI
 '''
 from visual import *
 from math import *
@@ -11,29 +13,29 @@ scene.center = (0,-4,0)
 scene.width = 400
 scene.height = 700
 
-massa1 = sphere(pos=(0,0,0), radius=0.5, color=color.red, opacity=0.7, make_trail=True)
-massa2 = sphere(pos=(0,0,0), radius=0.5, color=color.red, opacity=0.7, make_trail=True)
+mass1 = sphere(pos=(0,0,0), radius=0.5, color=color.red, opacity=0.7, make_trail=True)
+mass2 = sphere(pos=(0,0,0), radius=0.5, color=color.red, opacity=0.7, make_trail=True)
 
 base = box(pos=(0,-10,-2), size=(7,0.5,7))
-haste = box(pos=(0,-4,-2), size=(0.6,12,0.6))
-pitoco = cylinder(pos=(0,0,-2), radius=0.2, axis=(0,0,2), color=color.yellow)
-fio1 = curve(pos=[(0,0,0),(massa1.pos)], color=color.gray(0.5))
-fio2 = curve(pos=[(massa1.pos),(massa2.pos)], color=color.gray(0.5))
+wall = box(pos=(0,-4,-2), size=(0.6,12,0.6))
+bar = cylinder(pos=(0,0,-2), radius=0.2, axis=(0,0,2), color=color.yellow)
+line1 = curve(pos=[(0,0,0),(mass1.pos)], color=color.gray(0.5))
+line2 = curve(pos=[(mass1.pos),(mass2.pos)], color=color.gray(0.5))
 
-teta01 = -35.
-teta02 = 0.
+theta01 = 30.
+theta02 = 30.
 l = 4.5
 pi = 3.1415
-teta1 = (180-teta01)*pi/180
-teta2 = (180-teta02)*pi/180
+theta1 = (180-theta01)*pi/180
+theta2 = (180-theta02)*pi/180
 g = -9.81
 omega1 = 0.
 omega2 = 0.
-alfa1 = 0.
-alfa2 = 0.
+alpha1 = 0.
+alpha2 = 0.
 
-a = cos(teta1 - teta2)
-b = sin(teta1 - teta2)
+a = cos(theta1 - theta2)
+b = sin(theta1 - theta2)
 
 t = 0
 dt = 0.01
@@ -43,20 +45,20 @@ label1 = label()
 while True:
     rate(250)
     t += dt
-    alfa1 = (g/l*(sin(teta2)*a -2*sin(teta1)) - omega1**2*b*a -omega2**2*b) / (2-a**2)
-    alfa2 = omega1**2*b - g/l*sin(teta2) - alfa1*a
+    alpha1 = (g/l*(sin(theta2)*a -2*sin(theta1)) - omega1**2*b*a -omega2**2*b) / (2-a**2)
+    alpha2 = omega1**2*b - g/l*sin(theta2) - alpha1*a
 
-    omega1 += alfa1*dt
-    omega2 += alfa2*dt
+    omega1 += alpha1*dt
+    omega2 += alpha2*dt
 
-    teta1 += omega1*dt
-    teta2 += omega2*dt
+    theta1 += omega1*dt
+    theta2 += omega2*dt
 
-    massa1.pos =(l*sin(teta1), l*cos(teta1), 0)
-    massa2.pos =(l*sin(teta1) + l*sin(teta2), l*cos(teta1) + l*cos(teta2), 0)
+    mass1.pos =(l*sin(theta1), l*cos(theta1), 0)
+    mass2.pos =(l*sin(theta1) + l*sin(theta2), l*cos(theta1) + l*cos(theta2), 0)
 
-    fio1.pos =[(0,0,0), (massa1.pos)]
-    fio2.pos =[(massa1.pos), (massa2.pos)]
+    line1.pos =[(0,0,0), (mass1.pos)]
+    line2.pos =[(mass1.pos), (mass2.pos)]
 
     label1.pos = base.pos + vector(0,-1,0)
     label1.text = 'time : %.2f s' % t
