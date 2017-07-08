afa3a5249a651261f8879ebf820f2da364ef2497
edward <gyurse@gmail.com>
2017-07-08 22:44:54 +0900

    from linux

--- ROS/catkin_ws_kinetic/build/CTestTestfile.cmake
+++ ROS/catkin_ws_kinetic/build/CTestTestfile.cmake
@@ -5,14 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(velodyne-master/velodyne)
-subdirs(velodyne-master/velodyne_msgs)
-subdirs(car_can)
-subdirs(controller)
-subdirs(localization)
-subdirs(mcu_can)
-subdirs(motion_planner)
+subdirs(followbot)
 subdirs(basics)
-subdirs(loam_velodyne)
-subdirs(velodyne-master/velodyne_driver)
-subdirs(velodyne-master/velodyne_pointcloud)
