cb61bcf598c817faee92f0694ff9f1ca9df7ab70
lockdpwn <gyurse@gmail.com>
2017-07-31 14:09:12 +0900

    from linux

--- ROS/catkin_ws/src/dbw_mkz/dbw_mkz_parking_lot/src/veltest_ed.cpp
+++ ROS/catkin_ws/src/dbw_mkz/dbw_mkz_parking_lot/src/veltest_ed.cpp
@@ -1,3 +1,6 @@
+/*
+  c++ ==> ROS, mkz차량이 가제보상에서 움직이는지 cmd_vel등 여러 토픽을 사용해 테스트해 본 코드
+ */
 #include <iostream>
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
@@ -7,26 +10,25 @@ using namespace std;
 using namespace ros;
 
 int main(int argc, char *argv[]) {
+  init(argc, argv, "cmd_vel_test_ed");
+  NodeHandle nh;
 
-init(argc, argv, "cmd_vel_test_ed");
-NodeHandle nh;
+  Publisher pub_cmd_vel = nh.advertise<geometry_msgs::Twist>("/dyros/cmd_vel", 1);
 
-Publisher pub_cmd_vel = nh.advertise<geometry_msgs::Twist>("/dyros/cmd_vel", 1);
+  Rate loop_rate(50);
+  geometry_msgs::Twist tw;
 
-Rate loop_rate(50);
-geometry_msgs::Twist tw;
-
-tw.linear.x = 15;
-tw.angular.z = 0.0;
-
+  tw.linear.x = 1;
+  tw.angular.z = 0.0;
 
 
-while(ok()){
-pub_cmd_vel.publish(tw);
 
-loop_rate.sleep();
-}
+  while(ok()){
+    pub_cmd_vel.publish(tw);
+
+    loop_rate.sleep();
+  }
 
 
-return 0;
+  return 0;
 }
